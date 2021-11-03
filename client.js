0576aba391855c6343c98cfc5858b0c34865c333
Bosi Chen <chenbosi.bosi@gmail.com>
2021-11-03 17:20:45 -0400

    add hard-coded move commands

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+// IP: 165.227.47.243
+// PORT: 50541
 const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
@@ -10,8 +12,16 @@ const connect = function () {
   conn.on("connect", () => {
     // code that does something when the connection is first established
     console.log('Successfully connected to game server');
-    conn.write('Name: NNW');
+    conn.write('Name: nnw');
+    
+    // conn.write('Move: up');
+    // setTimeout(() => conn.write('Move: up'), 100);
+    // setInterval(() => conn.write('Move: up'), 100);
   });
+
+  // conn.on('connect', () => {
+  //   conn.write('Move: up');
+  // });
 
   return conn;
 };
