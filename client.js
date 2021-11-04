e1e618e9275f8275cb0cae7e6bd0ac0f4d5d05a2
Bosi Chen <chenbosi.bosi@gmail.com>
2021-11-03 20:00:25 -0400

    keep constant values separated

--- client.js
+++ client.js
@@ -1,10 +1,10 @@
-// IP: 165.227.47.243
-// PORT: 50541
+const { IP, PORT } = require('./constants');
+
 const net = require("net");
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
   // interpret incoming data as text
